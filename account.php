7367fb026a740dfe84a8c4270675818c8dbbd9a1
DungIT99 <nguyendung3099@gmail.com>
2019-06-17 10:29:33 +0700

    up test

---
+++ account.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+</head>
+<body>
+    
+<?php 
+include("./connect.php");
+$sql = mysqli_query($conn,"SELECT * FROM members");
+if(isset($_POST["search"])){
+    $search = $_POST["search"];
+    $query = mysqli_query($conn, "SELECT * FROM members WHERE email =='%$search%' or firstName =='%$search%'");
+if($query){
+    $sql = $query;
+}else{
+    $sql="";
+}
+}
+
+?>
+<div class="container">
+    
+   <input type="text" name="search" >
+   
+   <button type="submit" class="btn btn-sm btn-default">search</button>
+   
+    
+        
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>surName</th>
+            <th>email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($sql as $a): ?>
+        <tr>
+            <td><?php echo $a["id"] ?></td>
+            <td><?php $a["fisrtName"]?></td>
+            <td><?php $a["surName"]?></td>
+            <td><?php $a["email"]?></td>
+        </tr>
+<?php endforeach; ?>
+    </tbody>
+</table>
+
+    
+     
+    </form>
+    
+</div>
+
+</body>
+</html>
---
+++ members.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+</head>
+<body>
+    
+<?php 
+include("./connect.php");
+$errors=[];
+if(isset($_POST["submit"])){
+    $name = isset($_POST["name"])?$_POST["name"]:"";
+    $surName =isset($_POST["surName"])?$_POST["surName"]:"";
+    $email = isset($_POST["email"])?$_POST["email"]:"";
+    if($name ==""){
+        $errors["name"] =" not null";
+    }
+    if($surName ==""){
+        $errors["surName"] =" not null";
+    }
+    if($email ==""){
+        $errors["email"] =" not null";
+    }
+
+if(count($errors)==0){
+        $member = "INSERT INTO `members`(fisrtName,surName,email) VALUES('$name','$surName','$email')";
+        print_r($member);
+        $query = mysqli_query($conn,$member);
+        if($query){
+            echo " thanh cong";
+        }else{
+            echo "false";
+        }
+}
+}
+?>
+<div class="container">
+    
+    <form action="" method="POST" role="form">
+        <legend>Members</legend>
+        <?php if(count($errors)>0): foreach($errors as $key => $value):?>
+
+        <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <strong><?php echo $key ?></strong> <?php echo $value ?>
+        </div>
+        <?php  endforeach  ;?>
+        <?php endif ;?>
+
+        
+    
+        <div class="form-group">
+            <label for="">Name</label>
+            <input type="text" class="form-control" id="" placeholder="Input field" name ="name ">
+        </div>
+        <div class="form-group">
+            <label for="">surName</label>
+            <input type="text" class="form-control" id="" placeholder="Input field" name="surName">
+        </div>
+        <div class="form-group">
+            <label for="">Email</label>
+            <input type="text" class="form-control" id="" placeholder="Input field" name="email ">
+        </div>
+    
+        
+    
+        <button type="submit" class="btn btn-primary" name ="submit">Submit</button>
+        
+        <button type="button" class="btn btn-default"><a href="./account.php">go to account</a></button>
+        
+    </form>
+    
+</div>
+
+</body>
+</html>
